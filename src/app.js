d31aee9c333d406f3ce5fa9b1d1b90a97e145538
whj <739960552@qq.com>
2020-10-01 15:51:37 +0800

    button-group margin-left:-1 z-index=1 解决hover缺少边

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import Vue from  'vue'
 import Button from './button'
 import Icon from './Icon'
+import buttonGroup from './buttonGroup'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
+Vue.component('g-button-group',buttonGroup)
 
 new Vue({
   el:'#app',
   data:{
-    loading1:true,
-    loading2:true,
-    loading3:true,
+    loading1:false,
+    loading2:false,
+    loading3:false,
   }
 })
---
+++ src/buttonGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'button-group',
+    props: [],
+    components: {},
+
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+  .g-button-group{
+    display: inline-block;
+    border-radius: 0;
+    vertical-align: middle;
+    > .g-button{
+      border-radius: 0;
+
+      &:not(:first-child){
+        margin-left: -1px;
+      }
+
+      &:first-child{
+        border-bottom-left-radius: var(--border-radius);
+        border-top-left-radius: var(--border-radius);
+      }
+      &:last-child{
+        border-bottom-right-radius: var(--border-radius);
+        border-top-right-radius: var(--border-radius);
+      }
+
+      &:hover{
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
